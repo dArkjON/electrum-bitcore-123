c8ef4f1d1440cc2bb0975453200c082d163d9e35
dArkjON <info@darkjon.de>
2018-01-15 15:54:47 +0100

    fix

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAME_ROOT=electrum
+NAME_ROOT=electrum-bitcore
 PYTHON_VERSION=3.5.4
 
 # These settings probably don't need any change
@@ -25,7 +25,7 @@ for repo in electrum electrum-locale electrum-icons; do
 	git checkout master
 	cd ..
     else
-	URL=https://github.com/spesmilo/$repo.git
+	URL=https://github.com/dArkjON/$repo.git
 	git clone -b master $URL $repo
     fi
 done
